c9509a11353da80638a0ca48c6095569289fc6fa
unknown <javier_2001@live.com.sg>
2022-09-30 21:30:37 +0800

    Updated jinja test

--- jinja_test/app.py
+++ jinja_test/app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)  # name is passed as it is used to find the root node
 
 @app.route("/")
 def home():
-    username = "Your Mom amongus"
+    username = "Testing 123"
     suggestions = [
         {"task": "Sandwich", "desc":"Pls am hungri"},
-        {"task": "Do nothing","desc": "Why live?"}
+        {"task": "Do nothing","desc": "..."}
         ]
     return render_template("main.html", username=username, suggestions=suggestions) 
 
